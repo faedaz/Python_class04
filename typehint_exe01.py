0bcd6144af80793e7cef1a0d8301dcd25ee9e101
Matheus Windows <matheus.fnds20@gmail.com>
2025-08-06 12:42:43 -0300

    learning type hint

---
+++ typehint_exe01.py
@@ -0,0 +1,11 @@
+product: str = 'keyboard'
+product2: str = 'mouse'
+product3: str = 'monitor'
+
+products: list = []
+
+products.append(product)
+products.append(product2)
+products.append(product3)
+
+print(products)
---
+++ typehint_exe02.py
@@ -0,0 +1,25 @@
+product01: dict = {
+    'name': 'Monitor',
+    'price': 200,
+    'qtt': 3
+}
+
+product02: dict = {
+    'name': 'keyboard',
+    'price': 120,
+    'qtt': 9
+}
+
+product03: dict = {
+    'name': 'mouse',
+    'price': 90,
+    'qtt': 4
+}
+
+car_list: list = []
+
+car_list.append(product01)
+car_list.append(product02)
+car_list.append(product03)
+
+print(f"{car_list}\n")
