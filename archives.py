be133b7eca792f0aaacda23f9b27d91bd63bf6f2
Matheus Windows <matheus.fnds20@gmail.com>
2025-08-07 16:27:47 -0300

    add

--- archives.py
+++ archives.py
@@ -5,8 +5,7 @@ path_file: str = "example.csv"
 file_csv: list = []
 
 with open(file=path_file, mode="r", encoding='utf-8') as file:
-    csv_reader = csv.DictReader(file)
-    
+    csv_reader = csv.DictReader(file)    
     
     for row in csv_reader:
         file_csv.append(row)
